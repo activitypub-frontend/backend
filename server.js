62122b030b1b0cc1ad0fb0768c7678660b74c43d
Marvin Gaube <dev@marvingaube.de>
2019-05-07 14:31:05 +0200

    Fix lint errors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 require('dotenv').config();
-console.log(process.env.API_URL)
+console.log(process.env.API_URL);
 const express = require('express');
 const request = require('request');
 const btoa = require('btoa');
@@ -11,17 +11,17 @@ app.use(express.static('public'));
 
 // API-Endpoints
 app.post('/getTTS', (req, res) => {
-	request.post((process.env.WATSON_API_URL, {
-		headers: {
-			Authorization: 'Basic ' + btoa(process.env.WATSON_API_KEY),
-			Accept: 'audio/mpeg',
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			text: req.body.text,
-			accept: 'audio/mpeg'
-		})
-	}).pipe(res);
+  request.post(process.env.WATSON_API_URL, {
+    headers: {
+      'Authorization': 'Basic ' + btoa(process.env.WATSON_API_KEY),
+      'Accept': 'audio/mpeg',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      text: req.body.text,
+      accept: 'audio/mpeg',
+    }),
+  }).pipe(res);
 });
 
 app.post('/getFile', (req, res) => request.get(req.body.url).pipe(res));
