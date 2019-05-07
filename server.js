b496f1099337efb6b6f71d40c967048bdb654d3e
Marvin Gaube <dev@marvingaube.de>
2019-05-07 13:41:55 +0200

    Some design improvements

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const express = require('express');
 const request = require('request');
 const btoa = require('btoa');
 const app = express();
-app.use(express.json());
+
+// load submodule frontend as static
+app.use(express.static('public'));
+
+// API-Endpoints
 app.post('/getTTS', (req, res) => {
 	request.post('https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?voice=de-DE_BirgitVoice', {
 		headers: {
+
 			Authorization: 'Basic ' + btoa(''),
 			Accept: 'audio/mpeg',
 			'Content-Type': 'application/json'
@@ -18,5 +23,5 @@ app.post('/getTTS', (req, res) => {
 	}).pipe(res);
 });
 app.post('/getFile', (req, res) => request.get(req.body.url).pipe(res));
-app.use(express.static('public'));
+
 app.listen(5000, () => console.log('Listening on http://127.0.0.1:5000/'));
